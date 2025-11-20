60a38753622c02a030590d999179bbe5a83fc949
Dang Anh Que <160758822+cinnamoll@users.noreply.github.com>
2025-11-21 00:02:16 +0700

    them auth vao routes

--- backend/routes/pushRoutes.js
+++ backend/routes/pushRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { subscribePush, unsubscribePush } = require('../controllers/pushController')
+const { protect } = require('../middleware/auth')
+
+router.use(protect)
 
 router.post('/subscribe', subscribePush)
 
 router.delete('/unsubscribe', unsubscribePush)
 
 router.get('/vapid-public', (req, res) => {
-  res.send.json({
+  res.json({
     vapidPublicKey: process.env.VAPID_PUBLIC_KEY
   })
 })
